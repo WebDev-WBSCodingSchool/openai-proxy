45c881c94f12c4908801b1db98212a5fc394ed35
Bashar Alwarad <beelwarad52@gmail.com>
2025-02-12 14:32:34 +0100

    testing1

--- controllers/chats.js
+++ controllers/chats.js
@@ -1,36 +1,29 @@
 import OpenAI from 'openai';
-import OpenAIMock from '../utils/OpenAIMock.js';
 import asyncHandler from '../utils/asyncHandler.js';
 
 export const createChat = asyncHandler(async (req, res) => {
-  const {
-    body: { stream, ...request },
-    headers: { mode },
-  } = req;
+  const { messages } = req.body;
 
-  let openai;
-
-  mode === 'production'
-    ? (openai = new OpenAI({ apiKey: process.env.OPEN_AI_APIKEY }))
-    : (openai = new OpenAIMock());
+  const openai = new OpenAI({ apiKey: process.env.OPEN_AI_APIKEY });
 
   const completion = await openai.chat.completions.create({
-    stream,
-    ...request,
+    stream: false,
+    ...messages,
   });
+  res.json(completion.choices[0]);
 
-  if (stream) {
-    res.writeHead(200, {
-      Connection: 'keep-alive',
-      'Cache-Control': 'no-cache',
-      'Content-Type': 'text/event-stream',
-    });
-    for await (const chunk of completion) {
-      res.write(`data: ${JSON.stringify(chunk)}\n\n`);
-    }
-    res.end();
-    res.on('close', () => res.end());
-  } else {
-    res.json(completion.choices[0]);
-  }
+  // if (stream) {
+  //   res.writeHead(200, {
+  //     Connection: 'keep-alive',
+  //     'Cache-Control': 'no-cache',
+  //     'Content-Type': 'text/event-stream',
+  //   });
+  //   for await (const chunk of completion) {
+  //     res.write(`data: ${JSON.stringify(chunk)}\n\n`);
+  //   }
+  //   res.end();
+  //   res.on('close', () => res.end());
+  // } else {
+  //   res.json(completion.choices[0]);
+  // }
 });
